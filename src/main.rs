88737f2225b3468e615c05139958ebf8fdde0146
Remy Pierre Bushnell Clarke <>
2023-04-16 02:01:22 +0900

    getting started with setup

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,10 @@
+#![no_implicit_prelude]
+extern crate core;
+extern crate alloc;
+extern crate std;
+
+core::include!{"main_imports.rs"}
+
+core::include!{"memory.rs"}
+
 fn main() {}
---
+++ src/main_imports.rs
@@ -0,0 +1,4 @@
+use core::
+{ option::{Option::*}
+, clone::Clone
+};
